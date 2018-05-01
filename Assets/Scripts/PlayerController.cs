032a567df27a9a6da8623c993c02616caa4b0799
Marzanna77 <36212652+Marzanna77@users.noreply.github.com>
2018-05-01 02:28:06 -0400

    added some scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D Rigid; //public=seen in inspector  private=invisible in inspector   
+    //USE AND DECLARE SHIT HERE FROM NOW ON ^^^^^^^^^^^^
+    public float JumpForce; //float=decimals
+    //public int JumpForceInt; //int= whole number
+
+    //public Animator Anim;
+
+    //public GameObject GameOverText;
+	// Use this for initialization
+
+
+
+
+
+	void Start () {
+        //Rigid = gameObject.GetComponent<RigidBody2D>();  //use this if you declared it grabs the component inside the <>   
+        //dont use shit like this^ again!!!^^^^^^
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (Input.GetKeyDown(KeyCode.Space) == true)
+       // {
+            
+           // Anim.SetBool("SpaceCheck", true);
+            //Anim.SetBool("JumpDown", true);
+      //  }
+
+
+        if (Input.GetKey(KeyCode.Space) == true)
+        {
+            Rigid.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+           // Anim.SetBool("SpaceCheck", false);
+            //Anim.SetBool("JumpUp", true);
+        }
+
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+   // {
+        //if(collision.gameObject.tag == "Wall")
+       // {
+           // GameOverText.SetActive(true);
+       // }   
+   // }
+
+
+  
+}
---
+++ Assets/Scripts/TerrainSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSpawner : MonoBehaviour {
+
+    public GameObject TerrainPrefab;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(SpawnTerrain());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SpawnTerrain()
+    {
+        //Debug.Log("entered spawn wall"); //sending message to console
+        Instantiate(TerrainPrefab, transform.position, Quaternion.identity); //will spawn shit at spawner loaction
+
+        yield return new WaitForSeconds(1);
+
+        StartCoroutine(SpawnTerrain());
+
+       // Debug.Log("exited spawn wall"); //testing to make sure coroutine works
+    }
+
+
+
+}
